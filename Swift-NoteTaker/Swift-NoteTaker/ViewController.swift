28649d0239b170ad2b17f9593843fd3656a834cd
KRMeserve <krmeserve@gmail.com>
2020-01-22 22:04:08 -0500

    started work on figuring out how permanent storage on iphones work

---
+++ Swift-NoteTaker/Swift-NoteTaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-NoteTaker
+//
+//  Created by Kyle Meserve on 1/22/20.
+//  Copyright © 2020 Norien Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
