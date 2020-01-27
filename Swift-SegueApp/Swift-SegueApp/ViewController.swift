d1a9a9ac6e4529f2f8d42708234b2ce17e4893ea
KRMeserve <krmeserve@gmail.com>
2020-01-26 20:54:16 -0500

    starting work with more than one view controller - about to start with segues

---
+++ Swift-SegueApp/Swift-SegueApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Swift-SegueApp
+//
+//  Created by Kyle Meserve on 1/26/20.
+//  Copyright © 2020 Norien Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var labelScreenOne: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Swift-SegueApp/Swift-SegueApp/ViewControllerScreenTwo.swift
@@ -0,0 +1,22 @@
+//
+//  ViewControllerScreenTwo.swift
+//  Swift-SegueApp
+//
+//  Created by Kyle Meserve on 1/26/20.
+//  Copyright © 2020 Norien Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerScreenTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+  @IBOutlet weak var labelViewController2: UILabel!
+  
+
+}
