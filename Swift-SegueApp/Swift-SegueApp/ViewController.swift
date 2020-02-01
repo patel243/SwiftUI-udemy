2627eb2742860888c761a23e2e7d22856f7ac7b0
KRMeserve <krmeserve@gmail.com>
2020-02-01 09:42:04 -0500

    learned viewController lifecycle functions

--- Swift-SegueApp/Swift-SegueApp/ViewController.swift
+++ Swift-SegueApp/Swift-SegueApp/ViewController.swift
@@ -11,15 +11,41 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet weak var labelScreenOne: UILabel!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
   @IBOutlet weak var nameText: UITextField!
   
   var myName = ""
+  
+  // This is called first, but only the first time you go to this view. Afterwards, only calls viewWillAppear and viewDidAppear
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    // Lifecycle
+    print("viewDidLoad function called")
+  }
+  // Lifecycle testing
+  
+  // As soon as the view does disappear, this is called.
+  override func viewDidDisappear(_ animated: Bool) {
+    print("viewDidDisappear function called")
+  }
+  
+  // The moment you start leaving this view, this is called.
+  override func viewWillDisappear(_ animated: Bool) {
+    print("viewWillDisappear function called")
+  }
+
+  // This gets called immediately after the view loads
+  override func viewWillAppear(_ animated: Bool) {
+    print("viewWillAppear called")
+    nameText.text = ""
+  }
+  
+  // This gets called as soon as it appears on screen
+  override func viewDidAppear(_ animated: Bool) {
+    print("viewDidAppear called")
+  }
+  
+  
   
   @IBAction func nextButton(_ sender: Any) {
     myName = nameText.text!
