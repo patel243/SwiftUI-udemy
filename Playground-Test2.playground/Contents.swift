402e600462e97bbd199eb145166b39b9dd00f5fc
KRMeserve <krmeserve@gmail.com>
2020-01-31 21:20:14 -0500

    built function that can check if an array has duplicates. How useful.

--- Playground-Test2.playground/Contents.swift
+++ Playground-Test2.playground/Contents.swift
@@ -12,3 +12,32 @@ for word in arr {
 }
 
 print(newArr)
+var duplicateArray: [String] = ["Five", "words", "in", "this", "array", "it", "is", "a", "cool", "array"]
+
+func checkArrForDuplicates (array: [String]) -> String {
+  var wordsUsed: [String] = []
+  var duplicateWords: [String] = []
+  
+  for word in array {
+    if wordsUsed.contains(word) {
+      wordsUsed.append(word)
+      duplicateWords.append(word)
+      print(wordsUsed)
+      print(duplicateWords)
+    } else {
+      wordsUsed.append(word)
+    }
+    print(word)
+  }
+  
+  if duplicateWords.count > 0 {
+    print(duplicateWords)
+    return "Duplicate words detected!"
+  } else {
+    return "No duplicate words detected!"
+  }
+}
+
+checkArrForDuplicates(array: newArr)
+checkArrForDuplicates(array: duplicateArray)
+checkArrForDuplicates(array: arr)
