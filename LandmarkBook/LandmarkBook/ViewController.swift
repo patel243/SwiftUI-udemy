e3d343124578d8fe6458f87a38b059d2d283d44e
KRMeserve <krmeserve@gmail.com>
2020-02-02 10:57:58 -0500

    added functionality so names of each row are taken from our array. also added faux delete functionality

--- LandmarkBook/LandmarkBook/ViewController.swift
+++ LandmarkBook/LandmarkBook/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  // Can do this if we don't want to fill the array until the viewDidLoad
+  var landmarkNames = [String]()
+  var landmarkImages = [UIImage]()
   @IBOutlet weak var tableView: UITableView!
   
   override func viewDidLoad() {
@@ -18,17 +21,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // Sets ViewController up to be a UITableViewDelegate and UITableViewDataSource
     tableView.delegate = self
     tableView.dataSource = self
+    
+    
+    // Image setup. First make array of strings, then array of images (need to append the .png/.jpg if they're not in assets folder
+    
+    landmarkNames.append("Colusseum")
+    landmarkNames.append("Kremlin")
+    landmarkNames.append("Stonehenge")
+    landmarkNames.append("Taj Mahal")
+    
+    landmarkImages.append(UIImage(named: "colusseum.jpg")!)
+    landmarkImages.append(UIImage(named: "kremlin.jpg")!)
+    landmarkImages.append(UIImage(named: "stonehenge.png")!)
+    landmarkImages.append(UIImage(named: "tajMahal.jpg")!)
+    
   }
   
   // Functions imported from the table view delegate and table view data source
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    // deleting values from arrays
+    if editingStyle == .delete {
+      landmarkNames.remove(at: indexPath.row)
+      landmarkImages.remove(at: indexPath.row)
+      // delete the row with an animation
+      tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+    }
+  }
+  
+  
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell()
-    cell.textLabel?.text = "TEST"
+    // indexPath comes for free with tableView
+    cell.textLabel?.text = landmarkNames[indexPath.row]
     return cell
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return landmarkNames.count
   }
 
 }
